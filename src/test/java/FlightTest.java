c6475abde05167ba3f81049ecacd15392a5d1d76
Sergio <sergio.eneide@protonmail.com>
2021-08-28 11:44:49 +0100

    FLIGHT TEST hasDestination PASSED

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,17 +1,22 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.print.attribute.standard.Destination;
+
 import static org.junit.Assert.assertEquals;
 
 public class FlightTest {
 
     Plane plane;
     Flight flight;
+    Destination destination;
 
     @Before
     public void setUp(){
         plane = new Plane(250,10.000,PlaneType.BOEING767);
-        flight = new Flight(plane, "CCDNJ6");
+        flight = new Flight(plane, "CCDNJ6", "Barbados");
+
+
     }
 
     @Test
@@ -23,6 +28,8 @@ public class FlightTest {
         assertEquals("CCDNJ6",flight.getFlightNumber());
     }
 
-
-
+    @Test
+    public void hasDestination(){
+        assertEquals("Barbados", flight.getDestination());
+    }
 }
